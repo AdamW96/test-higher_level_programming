ecc7ce84e60f272f58f8e28cc91dda8c8800b8f1
adam.w <a.wang@groupeonepoint.com>
2025-06-27 15:46:07 +1000

    add task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers.
+
+    Args:
+        matrix: A 2D list of integers to print
+    """
+    for row in matrix:
+        for i, integer in enumerate(row):
+            if i > 0:
+                print(" ", end="")
+            print("{:d}".format(integer), end="")
+        print()
