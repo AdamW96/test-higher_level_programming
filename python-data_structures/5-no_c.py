c5542cdaa0655bc24508e1061fc3c6440e47e810
adam.w <a.wang@groupeonepoint.com>
2025-06-27 15:44:31 +1000

    add task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    """Remove all characters 'c' and 'C' from a string.
+
+    Args:
+        my_string: The input string to process
+
+    Returns:
+        A new string with all 'c' and 'C' characters removed
+    """
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
