3986e089815e3368389dc3607cb9ee285a6c25e5
adam.w <a.wang@groupeonepoint.com>
2025-06-27 15:40:05 +1000

    add task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list in reverse order, one per line.
+
+    Args:
+        my_list: List of integers to print in reverse
+    """
+    for integer in reversed(my_list):
+        print("{:d}".format(integer))
