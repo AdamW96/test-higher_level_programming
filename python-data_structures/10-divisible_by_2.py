28aa7f564a906a6e7314543c5ec9bc8f5829d7ee
adam.w <a.wang@groupeonepoint.com>
2025-06-27 15:55:15 +1000

    add task 12

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Find all multiples of 2 in a list.
+
+    Args:
+        my_list: List of integers to check
+
+    Returns:
+        A new list with True or False values indicating whether
+        each corresponding element is divisible by 2
+    """
+    result_list = []
+    for integer in my_list:
+        result_list.append(integer % 2 == 0)
+    return result_list
