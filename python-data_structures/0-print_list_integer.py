18781d3fcae0c0d0bc5203fc061440ba70499d9d
adam.w <a.wang@groupeonepoint.com>
2025-06-27 15:28:49 +1000

    add task 0 and task 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Print all integers of a list, one per line.
+
+    Args:
+        my_list: List of integers to print
+    """
+    for integer in my_list:
+        print("{}".format(integer))
--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def element_at(my_list, idx):
+    """Retrieve an element from a list at a given index.
+
+    Args:
+        my_list: The list to retrieve element from
+        idx: The index of the element to retrieve
+
+    Returns:
+        The element at the given index, or None if index is invalid
+    """
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    return my_list[idx]
