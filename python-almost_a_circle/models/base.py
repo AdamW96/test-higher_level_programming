e1bc0ceb6281292012146fe84025c5846cce07dd
adam.w <a.wang@groupeonepoint.com>
2025-07-08 21:19:50 +1000

    add to json string fnc

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 Base module.
 Contains the Base class which manages id attribute for all future classes.
 """
+import json
 
 
 class Base:
@@ -32,3 +33,34 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns JSON string representation of list_dictionaries
+
+        JSON is one of the standard formats for sharing data representation.
+        This method converts a list of dictionaries to a JSON string format
+        that can be easily stored, transmitted, or processed.
+
+        Args:
+            list_dictionaries (list): List of dictionaries to convert
+
+        Returns:
+            str: JSON string representation of the list
+                 Returns "[]" if list_dictionaries is None or empty
+
+        Examples:
+            Base.to_json_string([{"id": 1, "name": "test"}])
+            # Returns: '[{"id": 1, "name": "test"}]'
+
+            Base.to_json_string([])
+            # Returns: "[]"
+
+            Base.to_json_string(None)
+            # Returns: "[]"
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
