9583aa08c974e84c2d04dfccf79f8fc609c5cdfc
adam.w <a.wang@groupeonepoint.com>
2025-07-08 21:58:28 +1000

    fix 1 redirection issue

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to a URL and displays the body only for 200 status code responses
-curl -s -o /tmp/body -w "%{http_code}" "$1" | grep -q "200" && cat /tmp/body
+curl -s -L -o /tmp/body -w "%{http_code}" "$1" | grep -q "200" && cat /tmp/body
