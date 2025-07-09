9ce51c53d6a3cb8a8a77c43a10d7106b6f2ee340
adam.w <a.wang@groupeonepoint.com>
2025-07-09 10:41:10 +1000

    POST an email#1

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+# !/usr/bin/python3
+"""
+This script takes a URL and an email as arguments, sends a POST request
+to the URL with the email as a parameter using requests, and displays the response body.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    headers = {
+        'cfclearance': 'true'
+    }
+
+    data = {'email': email}
+    response = requests.post(url, data=data, headers=headers)
+    print(response.text)
