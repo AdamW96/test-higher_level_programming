5d0927234bfe2ebec6021fe0850a74672af036ee
adam.w <a.wang@groupeonepoint.com>
2025-07-09 10:27:36 +1000

    bypass firewall security

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,7 +7,9 @@ and displays the body response information.
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    req.add_header('cfclearance', 'true')
+    with urllib.request.urlopen(req) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
